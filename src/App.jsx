186c5cf0a9d97cf6f4f63e6d98453d4a64a8bd46
rajanhere <rajan@brainstackhub.com.au>
2024-02-08 13:51:47 +1100

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <>New Project! Coming Soon!</>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element= {<h1>New Project! Coming Soon!</h1>}>  </Route>
+      </Routes>
+    </>
+  );
+ 
 }
 
 export default App;
